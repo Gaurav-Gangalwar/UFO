36dd92da218c995cb9f8a2ed70809f30ef8a7408
Gaurav <gaurav@gluster.com>
2011-08-21 23:28:46 +0530

    UFO: Upgrade script merged.

--- upgrade.py
+++ upgrade.py
@@ -2,10 +2,16 @@
 
 import os, ast
 import simplejson as json
+import pickle
+import pdb
+from xattr import getxattr, setxattr
 from hashlib import md5
 from tempfile import mkdtemp
+from shutil import copyfile, rmtree
 from swift.common.utils import HASH_PATH_SUFFIX
 
+PICKLE_PROTOCOL = 2
+METADATA_KEY = 'user.swift.metadata'
 AUTH_ACCOUNT = ''
 ADMIN_URL = ''
 ADMIN_KEY = ''
@@ -20,16 +26,14 @@ def restore_user_data():
     fp = file("user_info", 'r+')
 
     while True:
-        user_data = fp.readline()
-        user_data = user_data.strip('\n')
-        cmd = 'gluster-object-add-user -K %s -A %s %s' %(ADMIN_KEY, ADMIN_URL,\
+       user_data = fp.readline()
+       user_data = user_data.strip('\n')
+       cmd = 'gluster-object-add-user -K %s -A %s %s' %(ADMIN_KEY, ADMIN_URL,
                                                          user_data)
-        if len(user_data) == 0:
-            break
-        
-        if os.system(cmd + '>>/dev/null'):
-            raise Exception('%s failed' %cmd)
-        
+       if len(user_data) == 0:
+               break
+       if os.system(cmd + '>>/dev/null'):
+           raise Exception('%s failed' %cmd)
     fp.close()
 
 
@@ -90,24 +94,80 @@ def store_user_info():
     fpi.close()
 
 
-def revert_changes():
-    cmd = 'mv %s/* %s ' %(TMP_DIR, AUTH_ACCOUNT)
-    os.system(cmd + '2>/dev/null')
+def read_metadata(path):
+    """
+Helper function to read the pickled metadata from a File/Directory .
 
-    cmd = 'mv %s/.* %s ' %(TMP_DIR, AUTH_ACCOUNT)
-    os.system(cmd + '2>/dev/null')
+:param path: File/Directory to read metadata from.
+
+:returns: dictionary of metadata
+"""
+    metadata = ''
+    key = 0
+    try:
+        while True:
+            metadata += getxattr(path, '%s%s' % (METADATA_KEY, (key or '')))
+            key += 1
+    except IOError:
+        pass
+    if metadata:
+        return pickle.loads(metadata)
+    else:
+        return metadata
+
+def write_metadata(path, metadata):
+    """
+Helper function to write pickled metadata for a File/Directory.
+
+:param path: File/Directory path to write the metadata
+:param metadata: metadata to write
+"""
+    metastr = pickle.dumps(metadata, PICKLE_PROTOCOL)
+    key = 0
+    while metastr:
+        setxattr(path, '%s%s' % (METADATA_KEY, key or ''), metastr[:254])
+        metastr = metastr[254:]
+        key += 1
+
+def copy_metadata(src, dst):
+    write_metadata(dst, read_metadata(src))
+
+
+def dup_dir_tree(src, dst, mask_dir=''):
+
+    copy_metadata(src, dst)
+
+    for (path, dirs, files) in os.walk(src):
+        obj_path = path.replace(src, '').strip('/')
+        if mask_dir:
+            dirs.remove(mask_dir)
+            mask_dir = ''
+        for i in dirs:
+            os.mkdir(os.path.join(dst, obj_path, i))
+            copy_metadata(os.path.join(src, obj_path, i),
+                          os.path.join(dst, obj_path, i))
+
+        for i in files:
+            copyfile(os.path.join(src, obj_path, i),
+                     os.path.join(dst, obj_path, i))
+            copy_metadata(os.path.join(src, obj_path, i),
+                          os.path.join(dst, obj_path, i))
+
+def revert_changes():
+    dup_dir_tree(TMP_DIR, AUTH_ACCOUNT)
+    rmtree(TMP_DIR)
 
 def clear_existing_data():
     global TMP_DIR
 
-    TMP_DIR = mkdtemp(dir='/tmp')
+    TMP_DIR = mkdtemp(dir=AUTH_ACCOUNT)
+    dup_dir_tree(AUTH_ACCOUNT, TMP_DIR, TMP_DIR.replace(AUTH_ACCOUNT, '').strip('/'))
 
-    cmd = 'mv %s/* %s ' %(AUTH_ACCOUNT, TMP_DIR)
-    os.system(cmd + '2>/dev/null')
-
-    cmd = 'mv %s/.* %s ' %(AUTH_ACCOUNT, TMP_DIR)
-    os.system(cmd + '2>/dev/null')
-
+    for (path, dirs, files) in os.walk(AUTH_ACCOUNT):
+        dirs.remove(TMP_DIR.replace(AUTH_ACCOUNT, '').strip('/'))
+        for i in dirs:
+            rmtree(os.path.join(AUTH_ACCOUNT, i))
+        break
 
 def unmount_tmp_dir():
     print 'Cleaning up temporary files and directories'
@@ -127,8 +187,8 @@ def encrypt(name):
 def upgrade_url(url):
     import re
     obj = re.match('(https://.*/v1/AUTH_)(.*)', url)
-    if obj == None:
-        print 'failed to upgrade services'
+    if obj == None or (obj.groups()[1] not in get_account_list()):
+        raise Exception('failed to upgrade services')
     else:
         return obj.groups()[0] + encrypt(obj.groups()[1])
 
@@ -171,11 +231,10 @@ def init():
 
 
 def main():
-    print '!!!UNSTABLE'
     print 'This script will upgrade auth info of Beta1 to Beta2,' \
           ' continue only if you were using Beta1 and installed Beta2. \n'\
           'You should be using the same auth volume for using the' \
-          ' existing auth data. In case of any failure old auth data \n' \
+          ' existing auth data. In case of any failure, old auth data \n' \
           'will be reverted back and you are supposed to clean and recreate it manually.'
     proceed = raw_input('Press c to continue:')
     if proceed != 'c':
@@ -196,3 +255,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
